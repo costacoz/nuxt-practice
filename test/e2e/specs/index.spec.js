2d38e598d03f6ad59521d10343e2ff7946c09dd5
Konstantin <buserandi@gmail.com>
2021-03-19 18:58:38 +0200

    fix: nightwatch config and changed test

--- test/e2e/specs/index.spec.js
+++ test/e2e/specs/index.spec.js
@@ -2,8 +2,7 @@ describe('Example test', () => {
   test('open app correctly', (browser) => {
     const main = browser.page.main()
     main.navigate()
-    main.assert.visible('@docButton')
-    main.assert.visible('@githubButton')
+    main.assert.elementPresent('.VuetifyLogo')
     browser.end()
   })
 })
