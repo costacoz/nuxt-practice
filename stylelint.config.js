64e1937f6d0e554356a85fb49252f63be3e28541
Konstantin <buserandi@gmail.com>
2021-03-20 18:03:11 +0200

    fix3

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,5 +2,5 @@ module.exports = {
   extends: ['stylelint-config-standard'],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
-  rules: { 'no-comma-dangle': 'off', 'comma-dangle': 'off' }
+  rules: {}
 }
